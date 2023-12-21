b7f13958d8d36d6a23961b758c9bba2cb7a424d8
Dewi Jones <dewigwynj@gmail.com>
2023-12-21 15:28:34 +0000

    Introduce PrivateKey generation

--- src/CSP.cpp
+++ src/CSP.cpp
@@ -4,3 +4,19 @@
 int CSP::generateKeys(){
     return 2;
 }
+
+///@brief getter for ElGamal AHE public key
+CryptoPP::ElGamalKeys::PublicKey CSP::getPublicKeyAHE()
+{
+    return ahe_PublicKey;
+}
+
+/// @brief Generates the Keys and populates the variables for AHE scheme
+/// @return Result status - true if successfull
+bool CSP::generateKeysAHE(){
+    ahe_Decryptor.AccessKey().GenerateRandomWithKeySize(rng, 2048);
+    ahe_PrivateKey = ahe_Decryptor.AccessKey();
+    ahe_Encryptor = CryptoPP::ElGamal::Encryptor(ahe_Decryptor);
+
+    return true;
+}
--- src/CSP.hpp
+++ src/CSP.hpp
@@ -1,14 +1,29 @@
 #pragma once
 #include <cryptopp/elgamal.h>
+#include <cryptopp/osrng.h>
+#include "Ratings.hpp"
+
 class CSP {
 
 int generateKeysFHE();
-int generateKeysAHE();
+bool generateKeysAHE();
 int encryptAHE(int input);
 int decryptAHE(int input);
+
+// Keep track of the AHE scheme (ElGamal)
+CryptoPP::AutoSeededRandomPool rng;
+CryptoPP::ElGamalKeys::PrivateKey ahe_PrivateKey;
+CryptoPP::ElGamalKeys::PublicKey ahe_PublicKey;
+CryptoPP::ElGamal::Decryptor ahe_Decryptor;
+CryptoPP::ElGamal::Encryptor ahe_Encryptor;
 
 public:
 
 int generateKeys();
+CryptoPP::ElGamalKeys::PublicKey getPublicKeyAHE();
+
+CSP(){ 
+    generateKeysAHE();
+}
 
 };
